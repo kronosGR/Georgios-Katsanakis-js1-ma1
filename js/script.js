8942b0077f82238d233d33bf38a3fd6d273dcc11
kronosGR <geo.elgeo@gmail.com>
2021-01-17 21:48:28 +0100

    Finished MA1
    Answered 8 Question.

--- js/script.js
+++ js/script.js
@@ -12,3 +12,61 @@ const cats = [
     }
 ];
 
+// Question 1
+const cat = {
+    complain: function() {
+        console.log("Meow!");
+    }
+}
+
+cat.complain();
+
+
+// Question 2
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+
+// Question 3
+heading.style.fontSize = "2em";
+
+
+// Question 4
+heading.classList.add("subheading");
+
+
+// Question 5
+const paragraphs = document.querySelectorAll("p");
+for (let i=0; i<paragraphs.length; i++){
+    paragraphs[i].style.color = "red";
+}
+
+
+// Question 6
+const resultsContainer = document.querySelector(".results");
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.background = "yellow";
+
+
+// Question 7
+function print(list){
+    for(let i=0; i<list.length; i++){
+        console.log(list[i].name);
+    }
+}
+print(cats);
+
+
+// Question 8
+function createCats(cats){
+    let htmlToAdd = "";
+    for(let i=0; i<cats.length; i++){
+        htmlToAdd += `<div>
+                        <h5>${cats[i].name}</h5>
+                        <p>${cats[i].hasOwnProperty('age') ? cats[i].age : "Age unknown" }</p>
+                      </div>`;
+    }
+    return htmlToAdd;
+}
+
+document.querySelector(".cat-container").innerHTML = createCats(cats);
